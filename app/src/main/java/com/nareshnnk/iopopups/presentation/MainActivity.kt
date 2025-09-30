9920690754e0027b32e4fb4d7fb7176fadf4526e
Naresh <nareshnath.nn@gmail.com>
2025-09-30 13:57:03 +0530

    Added Io Snackbar demo

--- app/src/main/java/com/nareshnnk/iopopups/presentation/MainActivity.kt
+++ app/src/main/java/com/nareshnnk/iopopups/presentation/MainActivity.kt
@@ -2,11 +2,37 @@ package com.nareshnnk.iopopups.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.nareshnnk.iopopups.R
+import com.nareshnnk.iopopups.databinding.ActivityMainBinding
+import com.nareshnnk.iopopups.presentation.customviews.SnackbarIo
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupSnackbar()
+        setupStickySnackbar()
+    }
+
+    private fun setupSnackbar() {
+        val snackbar = SnackbarIo(this, this)
+        binding.snackbarButton.setOnClickListener {
+            snackbar.show(binding.root, "Hello from SnackbarIo!")
+        }
+    }
+
+    private fun setupStickySnackbar() {
+        val stickySnackbar = SnackbarIo(this, this)
+        binding.snackbarStickyButton.setOnClickListener {
+            stickySnackbar.showSticky(
+                binding.root,
+                "This is a sticky SnackbarIo! Tap close to dismiss.",
+                "Close"
+            )
+        }
     }
 }
